097ea2a9946e68673430b656abfec60d31f2d6c2
Sadid Ahmed <ssadidahmed01@gmail.com>
2026-05-20 05:33:32 +0600

    Initialized Laravel 11 base framework and project configuration

---
+++ backend/config/cors.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'paths' => ['api/*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://127.0.0.1:5500',
+        'http://localhost:5500',
+        'http://localhost:3000',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
